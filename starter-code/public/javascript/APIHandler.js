0c6fd2f220e35ecb35db974127227bc6c2eab440
JoaoPedro13 <jpc130@gmail.com>
2019-11-24 04:23:10 +0000

    missing edit and new

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.instance = axios.create({
+      baseURL: `${this.BASE_URL}`
+    });
   }
 
   getFullList () {
-
+    return this.instance.get(`/characters`)
+    .then(response => {
+      return response.data;
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+  getOneRegister (id) {
+    return this.instance.get(`/characters/${id}`)
+    .then(response => Promise.resolve(response.data));
   }
 
-  getOneRegister () {
-
+  createOneRegister (data) {
+    return this.instance.post('/characters', {
+      ...data,
+      id: Math.floor(Math.random() * 100000)
+    })
+    .then(response => Promise.resolve(response.data));
   }
 
-  createOneRegister () {
-
+  updateOneRegister (id, data) {
+    return this.instance.patch(`/characters/${id}`, data)
+    .then(response => Promise.resolve(response.data));
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return this.instance.delete(`/characters/${id}`)
+    .then(response => Promise.resolve(response.data));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,16 +1,56 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const $characterContainer = document.querySelector('.characters-container');
+
+
+
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
-  });
-
+  document.getElementById('fetch-all').addEventListener('click', function (event) {    
+    
+    charactersAPI.getFullList()
+    .then(characters => {
+      $characterContainer.innerHTML = "";
+      //console.log(characters)
+      for (let character of characters) {
+        $characterContainer.innerHTML += 
+        `<div class="character-info">
+        <div class="name">Character Name:  ${character.name}</div>
+        <div class="occupation">Character Occupation:  ${character.occupation}</div>
+        <div class="cartoon">Is a Cartoon?:  ${character.cartoon}</div>
+        <div class="weapon">Character Weapon:  ${character.weapon}</div>
+        </div>`;
+      }
+    })
+    .catch(error => {
+      console.log("error getFullList ", error);
+    })
+  });  
+  
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const characterId = document.querySelector('[name=character-id]').value;
+    charactersAPI.getOneRegister(characterId)
+    .then(character => {
+      //console.log(characterId)
+      $characterContainer.innerHTML = "";
+      $characterContainer.innerHTML = 
+        `<div class="character-info">
+          <div class="name">Character Name:  ${character.name}</div>
+          <div class="occupation">Character Occupation:  ${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon?:  ${character.cartoon}</div>
+          <div class="weapon">Character Weapon:  ${character.weapon}</div>
+        </div>`;
+      })
+    .catch(error => {
+      console.log("error getOneRegister ", error);
+    })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const characterId = document.querySelector('[name=character-id-delete]').value;
+    charactersAPI.deleteOneRegister(characterId)
+    .catch(error => {
+      console.log("error deleteOneRegister ", error);
+    })
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
